4a8eaf783f7b43a1f861f8235fba9fb7d253e5c1
Himanshu <sangalhimanshu08@gmail.com>
2024-06-30 19:01:55 +0530

    reset template.sol

--- part2/staker/contracts/Template.sol
+++ part2/staker/contracts/Template.sol
@@ -15,39 +15,4 @@ contract MockERC20 is ERC20 {
 // We are using an external testing methodology
 contract EchidnaTemplate {
 
-    Staker stakerContract;
-    MockERC20 tokenToStake;
-
-    // setup
-    constructor() {
-        tokenToStake = new MockERC20("Token", "TOK");
-        stakerContract = new Staker(address(tokenToStake));
-    }
-
-    // function-level invariants
-    function testStake(uint256 _amount) public {
-        // Pre-condition
-        require(tokenToStake.balanceOf(address(this)) > 0);
-        // Optimization: amount is now bounded between [1, balanceOf(address(this))]
-        uint256 amount = 1 + (_amount % (tokenToStake.balanceOf(address(this))));
-        // State before the "action"
-        uint256 preStakedBalance = stakerContract.stakedBalances(address(this));
-        // Action
-        uint256 stakedAmount = stakerContract.stake(amount);
-        // Post-condition
-        assert(stakerContract.stakedBalances(address(this)) == preStakedBalance + stakedAmount); 
-    }
-
-    function testUnstake(uint256 _stakedAmount) public {
-        // Pre-condition
-        require(stakerContract.stakedBalances(address(this)) > 0);
-        // Optimization: amount is now bounded between [1, stakedBalance[address(this)]]
-        uint256 stakedAmount = 1 + (_stakedAmount % (stakerContract.stakedBalances(address(this))));
-        // State before the "action"
-        uint256 preTokenBalance = tokenToStake.balanceOf(address(this));
-        // Action
-        uint256 amount = stakerContract.unstake(stakedAmount);
-        // Post-condition
-        assert(tokenToStake.balanceOf(address(this)) == preTokenBalance + amount); 
-    }
 }
