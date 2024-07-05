a83b0d61ed253bc9b918eccb52af40794d5f1a40
Himanshu <sangalhimanshu08@gmail.com>
2024-07-05 17:44:46 +0530

    add swap invariant for swapping functionality

--- part4/contracts/practice/EchidnaTest.sol
+++ part4/contracts/practice/EchidnaTest.sol
@@ -37,39 +37,63 @@ contract EchidnaTest is Setup {
         }
     }
 
-    function testRemoveLiquidity(uint liquidityToRemove) public {
-        
-        uint lpTokenBalance = pair.balanceOf(address(user));
+    // function testRemoveLiquidity(uint liquidityToRemove) public {
+    //     uint lpTokenBalance = pair.balanceOf(address(user));
+    //     require(lpTokenBalance > 0 , "not enough liquidity");
+    //     (uint reserve0Before, uint reserve1Before) = 
+    //     UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
+    //     uint kBefore = reserve0Before * reserve1Before;
+    //     liquidityToRemove = _between(liquidityToRemove, 1, lpTokenBalance);
 
-        require(lpTokenBalance > 0 , "not enough liquidity");
+    //     (bool success1,) = user.proxy(address(pair),abi.encodeWithSelector(pair.approve.selector,address(uniswapRouter),uint(-1)));
+    //     require(success1);
+
+    //     (bool success, ) = 
+    //     user.proxy(address(uniswapRouter), abi.encodeWithSelector(
+    //         uniswapRouter.removeLiquidity.selector, 
+    //         address(testToken1), 
+    //         address(testToken2),
+    //         liquidityToRemove,
+    //         0, 0, address(user), uint(-1)));
+
+    //     if (success) {
+    //         uint lpTokenAfter = pair.balanceOf(address(user));
+    //         (uint reserve0After, uint reserve1After) = 
+    //         UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
+    //         uint kAfter = reserve0After * reserve1After;
+    //         assert(lpTokenAfter <= lpTokenBalance);
+    //         assert(kAfter <= kBefore);
+    //     }    
+    // }
+
+    function testSwapTokens(uint amount0) public {
+
+        require(amount0 > 0 , "Amount swapped should be gt than zero");
+
+        address[] memory path = new address[](2);
+        path[0] = address(testToken1);
+        path[1] = address(testToken2);
 
         (uint reserve0Before, uint reserve1Before) = 
         UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
-
         uint kBefore = reserve0Before * reserve1Before;
 
-        liquidityToRemove = _between(liquidityToRemove, 1, lpTokenBalance);
+        uint balanceTestToken1 = testToken1.balanceOf(address(user));
+        uint balanceTestToken2 = testToken2.balanceOf(address(user));
 
-        (bool success1,) = user.proxy(address(pair),abi.encodeWithSelector(pair.approve.selector,address(uniswapRouter),uint(-1)));
-        require(success1);
-
-        (bool success, ) = 
-        user.proxy(address(uniswapRouter), abi.encodeWithSelector(
-            uniswapRouter.removeLiquidity.selector, 
-            address(testToken1), 
-            address(testToken2),
-            liquidityToRemove,
-            0, 0, address(user), uint(-1)));
+        (bool success,) = user.proxy(address(uniswapRouter), abi.encodeWithSelector(uniswapRouter.swapExactTokensForTokens.selector, amount0, 0, path, address(user), uint(-1)));
 
         if (success) {
-            uint lpTokenAfter = pair.balanceOf(address(user));
             (uint reserve0After, uint reserve1After) = 
             UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
+            uint kAfter= reserve0After * reserve1After;
 
-            uint kAfter = reserve0After * reserve1After;
-            assert(lpTokenAfter <= lpTokenBalance);
-            assert(kAfter <= kBefore);
-        }    
+            uint balanceAfterTestToken1 = testToken1.balanceOf(address(user));
+            uint balanceAfterTestToken2 = testToken2.balanceOf(address(user));
+
+            assert(kAfter > kBefore);
+            assert(balanceAfterTestToken1 < balanceTestToken1);
+            assert(balanceAfterTestToken2 > balanceTestToken2);
+        }
     }
-
 }
