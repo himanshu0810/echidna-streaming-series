c47a4a8a014779a8e9a4cdaa5cbe1e24e451eefd
Himanshu <sangalhimanshu08@gmail.com>
2024-07-06 15:47:53 +0530

    add test independence path functionality

--- part4/contracts/practice/EchidnaTest.sol
+++ part4/contracts/practice/EchidnaTest.sol
@@ -5,6 +5,8 @@ import "../libraries/UniswapV2Library.sol";
 import "../uni-v2/UniswapV2Router01.sol";
 
 contract EchidnaTest is Setup {
+    using SafeMath for uint;
+
     // Modify the test provide liquidity function using the periphery
     function testProvideLiquidity(uint amount0, uint amount1) public {
 
@@ -37,34 +39,34 @@ contract EchidnaTest is Setup {
         }
     }
 
-    // function testRemoveLiquidity(uint liquidityToRemove) public {
-    //     uint lpTokenBalance = pair.balanceOf(address(user));
-    //     require(lpTokenBalance > 0 , "not enough liquidity");
-    //     (uint reserve0Before, uint reserve1Before) = 
-    //     UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
-    //     uint kBefore = reserve0Before * reserve1Before;
-    //     liquidityToRemove = _between(liquidityToRemove, 1, lpTokenBalance);
+    function testRemoveLiquidity(uint liquidityToRemove) public {
+        uint lpTokenBalance = pair.balanceOf(address(user));
+        require(lpTokenBalance > 0 , "not enough liquidity");
+        (uint reserve0Before, uint reserve1Before) = 
+        UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
+        uint kBefore = reserve0Before * reserve1Before;
+        liquidityToRemove = _between(liquidityToRemove, 1, lpTokenBalance);
 
-    //     (bool success1,) = user.proxy(address(pair),abi.encodeWithSelector(pair.approve.selector,address(uniswapRouter),uint(-1)));
-    //     require(success1);
+        (bool success1,) = user.proxy(address(pair),abi.encodeWithSelector(pair.approve.selector,address(uniswapRouter),uint(-1)));
+        require(success1);
 
-    //     (bool success, ) = 
-    //     user.proxy(address(uniswapRouter), abi.encodeWithSelector(
-    //         uniswapRouter.removeLiquidity.selector, 
-    //         address(testToken1), 
-    //         address(testToken2),
-    //         liquidityToRemove,
-    //         0, 0, address(user), uint(-1)));
+        (bool success, ) = 
+        user.proxy(address(uniswapRouter), abi.encodeWithSelector(
+            uniswapRouter.removeLiquidity.selector, 
+            address(testToken1), 
+            address(testToken2),
+            liquidityToRemove,
+            0, 0, address(user), uint(-1)));
 
-    //     if (success) {
-    //         uint lpTokenAfter = pair.balanceOf(address(user));
-    //         (uint reserve0After, uint reserve1After) = 
-    //         UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
-    //         uint kAfter = reserve0After * reserve1After;
-    //         assert(lpTokenAfter <= lpTokenBalance);
-    //         assert(kAfter <= kBefore);
-    //     }    
-    // }
+        if (success) {
+            uint lpTokenAfter = pair.balanceOf(address(user));
+            (uint reserve0After, uint reserve1After) = 
+            UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
+            uint kAfter = reserve0After * reserve1After;
+            assert(lpTokenAfter <= lpTokenBalance);
+            assert(kAfter <= kBefore);
+        }    
+    }
 
     function testSwapTokens(uint amount0) public {
 
@@ -95,5 +97,84 @@ contract EchidnaTest is Setup {
             assert(balanceAfterTestToken1 < balanceTestToken1);
             assert(balanceAfterTestToken2 > balanceTestToken2);
         }
+    }
+
+
+    // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+        uint amountInWithFee = amountIn.mul(997);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        amountOut = numerator / denominator;
+    }
+
+    // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
+        require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+        uint numerator = reserveIn.mul(amountOut).mul(1000);
+        uint denominator = reserveOut.sub(amountOut).mul(997);
+        amountIn = (numerator / denominator).add(1);
+    }
+
+    function testPathIndependenceForSwaps(uint x) public {
+        
+        if (!completed) {
+            _init(1_000_000_000, 1_000_000_000);
+        }
+
+        (uint reserve0, uint reserve1) = UniswapV2Library.getReserves(address(factory), address(testToken1), address(testToken2));
+
+        uint MIN_AMOUNT = 100;
+        uint userBalance = testToken1.balanceOf(address(user));
+
+        require(userBalance > MIN_AMOUNT);
+
+        x = _between(x, MIN_AMOUNT, uint(-1)/ 100);
+        x = _between(x, MIN_AMOUNT, userBalance);
+
+        require(reserve0 > MIN_AMOUNT, "Not enough reserve");
+        require(reserve1 > MIN_AMOUNT, "Not enough reserve");
+
+        uint yOut;
+        {
+            yOut = getAmountOut(x, reserve0, reserve1);
+            if (yOut == 0)
+                yOut = 1;
+            x = getAmountIn(yOut, reserve0, reserve1);        
+        }
+
+        address[] memory path12 = new address[](2);
+        path12[0] = address(testToken1);
+        path12[1] = address(testToken2);
+        address[] memory path21 = new address[](2);
+        path21[0] = address(testToken2);
+        path21[1] = address(testToken1);
+
+        bool success;
+        bytes memory retdata;
+        uint xOut;
+        uint[] memory amounts;
+
+        (success, retdata) = 
+        user.proxy(address(uniswapRouter), abi.encodeWithSelector(uniswapRouter.swapExactTokensForTokens.selector, x, 0, path12, address(user), uint(-1)));
+        if (!success)
+            return;
+
+        amounts = abi.decode(retdata, (uint[]));
+        yOut = amounts[1];
+
+        (success, retdata) = 
+        user.proxy(address(uniswapRouter), abi.encodeWithSelector(uniswapRouter.swapExactTokensForTokens.selector, yOut, 0, path21, address(user), uint(-1)));
+        if (!success)
+            return;
+
+        amounts = abi.decode(retdata, (uint[]));
+        xOut = amounts[1];
+
+        assert(xOut < x);
+        assert(((x - xOut) * 100) <= (3 * x));
     }
 }
